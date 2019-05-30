a6c18878bf2653645450e9b5459b629086e0a1bf
Daniil Novikov <dannovik30@gmail.com>
2019-05-30 17:29:54 +0300

    add slider

---
+++ src/plugins/slider/slider.js
@@ -0,0 +1,27 @@
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
+    }
+    // breakpoints: {
+    //     1024: {
+    //         slidesPerView: 4,
+    //         spaceBetween: 40,
+    //     },
+    //     768: {
+    //         slidesPerView: 3,
+    //         spaceBetween: 30,
+    //     },
+    //     640: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 20,
+    //     },
+    //     320: {
+    //         slidesPerView: 1,
+    //         spaceBetween: 10,
+    //     }
+    // }
+});
