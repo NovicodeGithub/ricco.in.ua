c82ce229f88f4193303a0ebd0418d46af5ced4aa
Daniil Novikov <dannovik30@gmail.com>
2019-06-01 22:34:51 +0300

    add all responsive start page

---
+++ src/plugins/responsive-footer/responsive-footer.js
@@ -0,0 +1,5 @@
+$(function() {
+    $('.footer-links-holder h6').click(function () {
+        $(this).parent().toggleClass('active');
+    });
+});
--- src/plugins/slider/slider.js
+++ src/plugins/slider/slider.js
@@ -11,20 +11,30 @@ var swiper = new Swiper('.swiper-container', {
             slidesPerView: 2,
             spaceBetween: 10,
         },
-        1170: {
-            slidesPerView: 2,
+        1024: {
+            slidesPerView: 4,
             spaceBetween: 10,
         },
-        768: {
+        880: {
             slidesPerView: 3,
-            spaceBetween: 30,
+            spaceBetween: 90,
         },
         640: {
             slidesPerView: 2,
             spaceBetween: 20,
         },
+
+        480: {
+            slidesPerView: 2,
+            spaceBetween: -40,
+        },
+
+        440: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+
         320: {
-            slidesPerView: 1,
             spaceBetween: 10,
         }
     }
