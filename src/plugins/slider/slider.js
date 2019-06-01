88e22a0b2f2c717e07bfb8c3a7abb8d96d944e3f
Daniil Novikov <dannovik30@gmail.com>
2019-06-01 12:17:25 +0300

    add responsive footer

--- src/plugins/slider/slider.js
+++ src/plugins/slider/slider.js
@@ -5,23 +5,27 @@ var swiper = new Swiper('.swiper-container', {
     navigation: {
         nextEl: '.button-next',
         prevEl: '.button-prev',
+    },
+    breakpoints: {
+        1322: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        1170: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        }
     }
-    // breakpoints: {
-    //     1024: {
-    //         slidesPerView: 4,
-    //         spaceBetween: 40,
-    //     },
-    //     768: {
-    //         slidesPerView: 3,
-    //         spaceBetween: 30,
-    //     },
-    //     640: {
-    //         slidesPerView: 2,
-    //         spaceBetween: 20,
-    //     },
-    //     320: {
-    //         slidesPerView: 1,
-    //         spaceBetween: 10,
-    //     }
-    // }
 });
